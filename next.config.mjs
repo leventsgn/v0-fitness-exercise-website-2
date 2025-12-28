35e92878868b5d0843f2ae4f010c0ed4c36cd116
leventsgn <130192268+leventsgn@users.noreply.github.com>
2025-12-28 10:39:17 +0000

    Fix next.config.mjs for static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.public.blob.vercel-storage.com",
+      },
+    ],
     unoptimized: true,
   },
- 
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
 }
 
 export default nextConfig
